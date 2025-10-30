c0154d260dbf0cffc17c54208b50144cce561419
Athira Lonappan <67268286+athira1402@users.noreply.github.com>
2025-10-31 02:11:28 +0530

    Delete Banking_Management_System directory

--- Banking_Management_System/include/session.h
+++
@@ -1,4 +0,0 @@
-#define CUSTOMER_SESSION_FILE "data/customer_sessions.dat"
-#define EMPLOYEE_SESSION_FILE "data/employee_sessions.dat"
-#define MANAGER_SESSION_FILE  "data/manager_sessions.dat"
-#define ADMIN_SESSION_FILE    "data/admin_sessions.dat"
